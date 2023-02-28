dbd43f2267f3f10573ea028c5fa39650886fc142
Filipeesf19 <filipeesf1995@gmail.com>
2023-02-28 23:02:01 +0000

    video number 87: useState objects

---
+++ Apps/02-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const UseStateObject = () => {
+  const [name, setName] = useState("peter");
+  const [age, setAge] = useState(24);
+  const [hobby, setHobby] = useState("read books");
+
+  const displayPerson = () => {
+    setName("john");
+    setAge(28);
+    setHobby("scream at the computer");
+  };
+
+  return (
+    <div>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h4>Enjoys : {hobby}</h4>
+      <button className="btn" onClick={displayPerson}>
+        show john
+      </button>
+    </div>
+  );
+};
+
+export default UseStateObject;
