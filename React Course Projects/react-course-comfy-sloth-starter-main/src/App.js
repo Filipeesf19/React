3dd8d984c3ffb185ec0dc2a327904aaaeecb23f1
Filipe Fernandes <filipe.fernandes@tqc.local>
2023-07-26 16:57:56 +0100

    confy store progressed

--- React Course Projects/react-course-comfy-sloth-starter-main/src/App.js
+++ React Course Projects/react-course-comfy-sloth-starter-main/src/App.js
@@ -20,17 +20,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:id" element={<SingleProduct />} />
-        <Route
-          path="/checkout"
-          element={
-            <PrivateRoute>
-              <Checkout />
-            </PrivateRoute>
-          }
-        />
+        <Route path="cart" element={<Cart />} />
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<SingleProduct />} />
+        <Route path="checkout" element={<Checkout />} />
         <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
